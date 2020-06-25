d68b184b3f103bf585ed5f2400a68d87bf4852c3
Anders Åslund <anders.aslund@teknoir.ai>
2020-06-25 16:15:32 +0200

    Multiselect

--- src/configure-device.js
+++ src/configure-device.js
@@ -2,48 +2,47 @@ module.exports = function (RED) {
     var fs = require('fs')
     var yaml = require("js-yaml");
     const Client = require('kubernetes-client').Client;
+    var client = new Client({version: '1.13'});
+    var namespace = process.env.NAMESPACE || "default";
+
+    var readYaml = (path, cb) => {
+        fs.readFile(require.resolve(path), 'utf8', (err, data) => {
+            if (err)
+                cb(err);
+            else
+                cb(null, yaml.safeLoad(data));
+        })
+    }
+    readYaml('./kubeflow.org_devices.yaml', (err, crd) => {
+        if (err) {
+            console.log(err);
+        } else {
+            // this.client.apis['apiextensions.k8s.io'].v1beta1.customresourcedefinitions.post({body: crd})
+            //     .catch(err => {
+            //         //
+            //         // API returns a 409 Conflict if CRD already exists.
+            //         //
+            //         if (err.statusCode !== 409) this.error(err);
+            //     });
+            client.addCustomResourceDefinition(crd);
+        }
+    });
 
     function ConfigureDevice(config) {
         RED.nodes.createNode(this, config);
         var node = this;
-        this.device = config.device;
+        this.devices = config.devices;
         this.onceDelay = 0.25 * 1000;
-        this.namespace = process.env.NAMESPACE || "default";
-
-        this.client = new Client({version: '1.13'})
-
-        var readYaml = (path, cb) => {
-            fs.readFile(require.resolve(path), 'utf8', (err, data) => {
-                if (err)
-                    cb(err);
-                else
-                    cb(null, yaml.safeLoad(data));
-            })
-        }
-        readYaml('./kubeflow.org_devices.yaml', (err, crd) => {
-            if (err) {
-                this.error(err);
-            } else {
-                // this.client.apis['apiextensions.k8s.io'].v1beta1.customresourcedefinitions.post({body: crd})
-                //     .catch(err => {
-                //         //
-                //         // API returns a 409 Conflict if CRD already exists.
-                //         //
-                //         if (err.statusCode !== 409) this.error(err);
-                //     });
-                this.client.addCustomResourceDefinition(crd);
-            }
-        });
-
+        this.namespace = namespace;
+        this.client = client;
 
         this.onceTimeout = setTimeout(function () {
             node.emit("input", {send: true});
         }, this.onceDelay);
 
-
         node.on('input', function (msg) {
             try {
-                var context = RED.util.parseContextStore(node.device);
+                var context = RED.util.parseContextStore(node.id);
                 var target = node.context()["global"];
 
                 if (msg.hasOwnProperty("send")) {
@@ -51,33 +50,38 @@ module.exports = function (RED) {
                         if (err) {
                             node.error(err, msg);
                         } else {
-                            this.client.apis['kubeflow.org'].v1.namespaces(node.namespace).devices(node.device).get()
-                                .catch(err => {
-                                    // console.log("Get Device Error: ", err);
-                                    node.error(err, msg);
-                                })
-                                .then(device => {
-                                    if (!device) {
-                                        node.error("There is no device by name: " + node.device + " in namespace: " + node.namespace);
-                                    } else {
-                                        // console.log(device);
-                                        try {
-                                            device.body.spec.manifest.apps.items = val;
-                                        } catch (err) {
+                            if (!val) {
+                                node.error("There is no config to deploy, please connect some configuration nodes");
+                            } else {
+                                node.devices.forEach(deviceName => {
+                                    this.client.apis['kubeflow.org'].v1.namespaces(node.namespace).devices(deviceName).get()
+                                        .catch(err => {
+                                            // console.log("Get Device Error: ", err);
                                             node.error(err, msg);
-                                        }
+                                        })
+                                        .then(device => {
+                                            if (!device) {
+                                                node.error("There is no device by name: " + deviceName + " in namespace: " + node.namespace);
+                                            } else {
+                                                // console.log(device);
+                                                try {
+                                                    device.body.spec.manifest.apps.items = val;
+                                                } catch (err) {
+                                                    node.error(err, msg);
+                                                }
 
-                                        this.client.apis['kubeflow.org'].v1.namespaces(node.namespace).devices(node.device).put(device)
-                                            .catch(err => {
-                                                // console.log("Update Device Error: ", err);
-                                                node.error(err, msg);
-                                            })
-                                            // .then(update => {
-                                            //     console.log("Update: ", update);
-                                            // })
-                                    }
-                                })
-
+                                                this.client.apis['kubeflow.org'].v1.namespaces(node.namespace).devices(deviceName).put(device)
+                                                    .catch(err => {
+                                                        // console.log("Update Device Error: ", err);
+                                                        node.error(err, msg);
+                                                    })
+                                                // .then(update => {
+                                                //     console.log("Update: ", update);
+                                                // })
+                                            }
+                                        })
+                                });
+                            }
                         }
                     });
                 } else {
@@ -125,5 +129,17 @@ module.exports = function (RED) {
             this.error(err.message);
         }
     };
+
+    RED.httpAdmin.get('/node-red-teknoir-configure-devices', function (req, res) {
+        client.apis['kubeflow.org'].v1.namespaces(namespace).devices().get()
+            .catch(error => res.status(500).send(error))
+            .then(devices => {
+                names = [];
+                devices.body.items.forEach((device) => {
+                    names.push(device.metadata.name);
+                })
+                res.status(200).json(names);
+            })
+    });
 
 }
